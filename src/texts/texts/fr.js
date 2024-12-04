a2a5161d42309ad209ce81920114b823ead5e2e1
timmeyer1 <timvameyer@gmail.com>
2024-12-04 17:11:15 +0100

    add readme

--- src/texts/texts/fr.js
+++ src/texts/texts/fr.js
@@ -54,7 +54,7 @@ export const textsFr = [
 
   "Minecraft, développé par Mojang en 2009, est un jeu de construction en monde ouvert qui a rapidement capturé l'imaginaire des joueurs grâce à sa simplicité, son monde pixelisé et ses possibilités infinies.",
 
-  `Dans le premier jeu The Legend of Zelda (1986), vous pouviez commencer le jeu sans aucune arme. `,
+  `Dans le premier jeu The Legend of Zelda (1986), vous pouviez commencer le jeu sans aucune arme.`,
 
   "L'e-sport est un secteur en pleine expansion qui regroupe des compétitions de jeux vidéo au niveau professionnel. Des jeux comme League of Legends, Dota 2, Counter-Strike et Fortnite attirent des millions de spectateurs, tandis que les meilleurs joueurs professionnels sont devenus de véritables célébrités.",
 
